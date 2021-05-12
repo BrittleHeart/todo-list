b5362eb0584c1650ff73ad11ea49db78d28d9c73
Bartosz Pazdur <b.pazdur@onet.pl>
2021-05-12 18:14:23 +0200

    Added middlewares functionality

--- back-end/app/http/Routes.ts
+++ back-end/app/http/Routes.ts
@@ -16,22 +16,30 @@ export default class Routes {
 	 * @param { string } method
 	 * @param { Router } router
 	 * @param { Function } controller
+	 * @param { any } middlewares
 	 * @returns { Router } router
 	 * eslint-disable
 	 */
-	public static async addRoute(method: Methods, route: string, controller: Function): Promise<Router> {
+	public static async addRoute(
+		method: Methods,
+		route: string,
+		controller: Function,
+		middlewares?: any
+	): Promise<Router> {
 		switch (method) {
 			case Methods.GET:
-				Routes.router.get(route, (request: Request, response: Response) => controller(request, response))
+				Routes.router.get(route, middlewares, (request: Request, response: Response) => controller(request, response))
 				break
 			case Methods.POST:
-				Routes.router.post(route, (request: Request, response: Response) => controller(request, response))
+				Routes.router.post(route, middlewares, (request: Request, response: Response) => controller(request, response))
 				break
 			case Methods.UPDATE:
-				Routes.router.put(route, (request: Request, response: Response) => controller(request, response))
+				Routes.router.put(route, middlewares, (request: Request, response: Response) => controller(request, response))
 				break
 			case Methods.DELETE:
-				Routes.router.delete(route, (request: Request, response: Response) => controller(request, response))
+				Routes.router.delete(route, middlewares, (request: Request, response: Response) =>
+					controller(request, response)
+				)
 				break
 		}
 
--- back-end/routes/api.ts
+++ back-end/routes/api.ts
@@ -2,24 +2,23 @@
  * main routing file. Here you can add / modify already existing routes.
  * Routes class has
  *
- * @see addRoute()
+ * @see Routes.addRoute()
  *
  * that you can use to scale your next best idea!
  */
 
 import { Request, Response } from 'express'
-import { FieldPacket, QueryError } from 'mysql2'
+import UserController from '../app/http/controllers/UserController'
 import Routes from '../app/http/Routes'
 import Methods from '../app/interfaces/MethodEnum'
-import QueryBuilderProvider from '../app/providers/QueryBuilder'
 
-const queryBuilder = new QueryBuilderProvider()
+Routes.addRoute(
+	Methods.GET,
+	'/users',
+	(req: Request, res: Response) => {
+		const user: UserController = new UserController(req, res)
 
-Routes.addRoute(Methods.GET, '/users', (req: Request, res: Response) => {
-	queryBuilder.select('select * from users', [], (err: QueryError | null, result: any, fields: FieldPacket[]) => {
-		if (err) throw new Error(err.message)
-
-		return res.send({ result, fields })
-	})
-	res.send('<h1>Youre close to me</h1>')
-})
+		return user.index()
+	},
+	[]
+)
