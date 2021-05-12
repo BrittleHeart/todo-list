628d8525a75c672d866f1c77b172a42288e9d181
Bartosz Pazdur <b.pazdur@onet.pl>
2021-05-12 11:10:39 +0200

    Added database classes and env util

--- back-end/server.ts
+++ back-end/server.ts
@@ -4,10 +4,29 @@ import Kernel from './app/Kernel'
 // Loading environment variables
 dotenv.config()
 
+declare global {
+	/* eslint-disable-next-line */
+	function env(value: string, defaultVal: string | number): string | undefined
+}
+
+/**
+ * global helper function for taking environment variables
+ *
+ * @param { string } value
+ * @param { string | number } defaultVal
+ * @returns string | undefined
+ */
+const _global = global /* node */ as any
+_global.env = function (value: string, defaultVal: string): string | undefined {
+	return process.env[value] ? process.env[value] : defaultVal
+}
+
 const PORT: number | any = process.env.PORT || process.env.APP_PORT
 
 // bring up kernel loading
 const application: Kernel = new Kernel(PORT)
+
+// global require('./utils')
 
 // loading routing
 require('./routes/api')
