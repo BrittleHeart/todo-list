d3a42e2b31a032868d3306a2f011439323001d3e
Bartosz Pazdur <b.pazdur@onet.pl>
2021-05-16 15:20:01 +0200

    Fixed invalid validation for todos create

--- back-end/app/http/controllers/TodoController.ts
+++ back-end/app/http/controllers/TodoController.ts
@@ -4,16 +4,35 @@ import QueryBuilderProvider from '../../providers/QueryBuilder'
 import Controller from './Controller'
 import ValidatorProvider from '../../providers/ValidatorProvider'
 import * as yup from 'yup'
+import { ObjectShape } from 'yup/lib/object'
 
 class TodoController extends Controller {
 	private queryBuilder: QueryBuilderProvider
 	private validator: ValidatorProvider
+	private shape: ObjectShape
 
 	constructor(response: Response, request: Request) {
 		super(response, request)
 
 		this.queryBuilder = new QueryBuilderProvider()
 		this.validator = new ValidatorProvider(request, response)
+
+		// shape of required request
+		this.shape = {
+			name: yup
+				.string()
+				.min(3)
+				.trim()
+				.max(255)
+				.matches(/[a-zA-Z0-9]{3,}/)
+				.required(),
+			description: yup
+				.string()
+				.trim()
+				.matches(/[a-zA-Z0-9]{3,}/)
+				.nullable(),
+			is_completed: yup.bool().optional().default(false),
+		}
 	}
 
 	/* eslint-disable */
@@ -65,38 +84,37 @@ class TodoController extends Controller {
 		const description: string = this.input('description') ? escape(this.input('description')) : ''
 		const is_completed = this.input('is_completed') !== undefined ? escape(this.input('is_completed')) : false
 
-		/**
-		 *
-		 */
-		const shape = {
-			name: yup
-				.string()
-				.min(3)
-				.trim()
-				.max(255)
-				.matches(/[a-zA-Z0-9]{3,}/)
-				.required(),
-			description: yup
-				.string()
-				.trim()
-				.matches(/[a-zA-Z0-9]{3,}/)
-				.nullable(),
-			is_completed: yup.bool().optional().default(false),
-		}
-
-		try {
-			await validate(shape, this.request.body)
-		} catch (error) {
-			return this.status(400).json({ status: error.name, message: error.message })
-		}
-
-		this.queryBuilder.execute(
-			'Insert into todos (name, description, is_completed) values (?,?,?)',
-			[name, description, is_completed],
-			(err: QueryError | null) => {
+		return this.queryBuilder.execute(
+			'select name from todos where name = ?',
+			[name],
+			async (err: QueryError | null, result: OkPacket | any) => {
+				// looking for errors
 				if (err) return this.status(400).send(`Something went wrong - ${err.message}`)
 
-				return this.status(201).json({ name, description, is_completed })
+				// check if record has been found
+				if (result.length > 0)
+					return this.status(400).json({
+						status: 400,
+						message: `Could not create todo with name = ${name}. The record already exists`,
+					})
+
+				// record validation
+				try {
+					await validate(this.shape, this.request.body)
+				} catch (error) {
+					return this.status(400).json({ status: error.name, message: error.message })
+				}
+
+				// excetuing the query
+				this.queryBuilder.execute(
+					'Insert into todos (name, description, is_completed) values (?,?,?)',
+					[name, description, is_completed],
+					(err: QueryError | null) => {
+						if (err) return this.status(400).send(`Something went wrong - ${err.message}`)
+
+						return this.status(201).json({ name, description, is_completed })
+					}
+				)
 			}
 		)
 	}
