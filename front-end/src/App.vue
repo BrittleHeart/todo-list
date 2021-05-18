242b54a54fc08574fd50610c9f21a31295087f33
Bartosz Pazdur <b.pazdur@onet.pl>
2021-05-18 15:14:07 +0200

    Styled input form

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,18 +1,21 @@
 <template>
-  <header class="app__heading">
+  <header class="app__heading" role="banner" aria-label="application header" aria-labelledby="Application header">
     <h1 class="app__header">simplest todo-list</h1>
     <p class="app__desc">save your do's here</p>
   </header>
-  <div class="wrapper">
-    <section class="app__authenticate" role="banner">
+
+  <section class="app__wrapper" role="application" aria-label="Main application core"
+           aria-labelledby="Main application core">
+    <article class="app__authenticate" role="article">
       <auth-banner/>
-    </section>
-  </div>
+    </article>
+  </section>
 </template>
 
 <script lang="ts">
 import {Options, Vue} from "vue-class-component"
 import AuthBanner from "./components/AuthBanner.vue"
+import './index.css'
 
 @Options({
   components: {
@@ -24,31 +27,17 @@ export default class App extends Vue {
 </script>
 
 <style>
-*, *::before, *::after {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
-
-body {
-  min-width: 100vw;
-  background-color: #f9f9f9f9;
-}
-
-body, html {
-  max-height: 100vh;
-}
-
 #app {
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
   height: 100vh;
+  padding: 0 10px;
 }
 
-.wrapper {
-  width: 30%;
+.app__wrapper {
+  width: 100%;
   margin: 0 auto;
   background-color: #ffffff;
   padding: 30px;
@@ -65,17 +54,17 @@ body, html {
   font-family: Poppins, sans-serif;
   font-weight: 600;
   text-transform: uppercase;
-  font-size: 35px;
+  font-size: 30px;
   line-height: 1.5;
 }
 
 .app__desc {
   font-family: 'Open Sans', sans-serif;
   font-weight: 300;
-  font-size: 18px;
+  font-size: 17px;
 }
 
-.wrapper .app__authenticate{
+.app__wrapper .app__authenticate {
   text-align: center;
 }
 </style>
--- front-end/src/components/AuthBanner.vue
+++ front-end/src/components/AuthBanner.vue
@@ -1,5 +1,7 @@
 <template>
-  <h2 class="auth-banner__header">Sign in</h2>
+  <section class="user-avatar">
+    <img src="@/assets/blank_avatar.png" alt="avatar image">
+  </section>
 
   <form @submit.prevent="signIn">
     <div class="form-group">
@@ -45,5 +47,41 @@ export default class AuthBanner extends Vue {
 </script>
 
 <style scoped>
+  .user-avatar img {
+    width: 8rem;
+    margin-bottom: 20px;
+  }
 
+  .form-group {
+    margin: 10px 0;
+  }
+
+  .form-group label {
+    display: none ;
+  }
+
+  .form-group input {
+    padding: 8px 7px;
+    width: 100%;
+    outline: none;
+    border: 1px solid rgba(40, 32, 32, 0.7);
+    transition: border 0.3s ease-in;
+    background: none;
+    font-family: 'Open Sans', sans-serif;
+    border-radius: 3px;
+    margin-bottom: 10px;
+  }
+
+  .form-group input:focus {
+    border: 2px solid rgba(145, 145, 250, 0.8);
+  }
+
+  ::placeholder {
+    color: rgb(0, 0, 0);
+    opacity: 1;
+  }
+
+  .form-group input:focus {
+
+  }
 </style>
